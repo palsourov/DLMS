1dfdff278539fd189ceca6cd77ef40009712d419
palsourov <palsourov365@gmail.com>
2024-05-07 03:24:08 +0600

    Add Code(Math.php)  & Data add to (mathBookImg folder)

--- Math.php
+++ Math.php
@@ -121,7 +121,200 @@
     </section>
  <!-- CALCULUS BOOKS END -->
 
- 
+ <!-- DISCRIET MATHEMATIC END   -->
+ <section class="book_section">
+      <h1 style="text-align: center; margin-top: 70px;"> DISCTIET MATHEMATIC BOOKS</h1>
+      <p style="text-align:center">Location: SeL:3 ,colum:8</p>
+      <div class="container book">
+            
+        <div class="row">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/DM1.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/DM2.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/DM3.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/DM4.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/DM5.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/DM6.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+              
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        
+    </section>
+ <!-- DISCRIET MATHEMATIC END  -->
+
+ <!-- DIFFERENCE MATHS BOOKS START -->
+ <section class="book_section">
+      <h1 style="text-align: center; margin-top: 70px;">Mental Math Books</h1>
+      <p style="text-align:center">Location: SeL:9 ,colum:18</p>
+      <div class="container book">
+            
+        <div class="row">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/M1.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m6.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m2.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m8.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m4.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m7.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+              
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m5.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m8.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/m9.jpeg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+ <!-- DIFFERENCE MATH BOOKS END -->
 
 
  <!-- MATHBooks END -->
