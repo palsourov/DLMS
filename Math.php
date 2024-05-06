3b1016e1027d6f9aec335da487f33b357ea36aa6
palsourov <palsourov365@gmail.com>
2024-05-07 02:51:14 +0600

    add(mathBookImg folder )

---
+++ Math.php
@@ -0,0 +1,134 @@
+<!-- THIS IS HEADER SECTION -->
+<?php 
+        require_once("header.php");
+    ?>
+<!-- THIS IS HEADER SECTION END-->
+
+<!-- MATHBooks START -->
+
+<!-- CalculusBooks -->
+    <section class="book_section">
+      <h1 style="text-align: center; margin-top: 70px;"> Calculus Books</h1>
+      <p style="text-align:center">Location: SeL:2 ,colum:4</p>
+      <div class="container book">
+            
+        <div class="row">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c1.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/cal.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c3.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c4.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c5.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c6.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+              
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c7.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c8.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="./img./mathBookImg/c9.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+ <!-- CALCULUS BOOKS END -->
+
+ 
+
+
+ <!-- MATHBooks END -->
+  
+<!-- THIS IS FOOTER SECTION -->
+  <?php 
+        require_once("footer.php");
+    ?>
+<!-- THIS IS FOOTER SECTION -->
+ 
