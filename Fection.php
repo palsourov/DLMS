5ae87e139a2c9cf1c1f3264301da8f015f0ca825
palsourov <palsourov365@gmail.com>
2024-05-07 13:08:41 +0600

    update code (Fection.php)

--- Fection.php
+++ Fection.php
@@ -4,95 +4,196 @@
     ?>
 <!-- THIS IS HEADER SECTION END-->
 
-<!-- PROGRAMMING BOOK SECTION -->
-    
-    <section class="book_section">
-      <h1 style="text-align: center; margin-top: 70px;">Most Popular Books</h1>
+<!--FECTIONBOOKS START -->
+  <section class="book_section">
+      <h1 style="text-align: center; margin-top: 70px;">Fection Books</h1>
+      <p style="text-align:center">Location: SeL:8 ,row:6</p>
       <div class="container book">
             
         <div class="row">
           <div
-            class="col-md-4 d-flex justify-content-center align-items-center"
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img/progamming.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkZgcG5SUlDv5_xSPqEUhvjwQK46mADapZtNbmrr6_vw&s" class="card-img-top" alt="..." />
               <div class="card-body">
-                <p class="card-text">
-                  30 Different Types of programming books...
-                </p>
+               
               </div>
-              <div class="btn btn-outline-secondary">View More</div>
+              
             </div>
           </div>
           <div
-            class="col-md-4 d-flex justify-content-center align-items-center"
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img/Science.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiF4a85TSCY0LHK9CjV_APTHioFBJgL_vyZeBuyvS-6g&s" class="card-img-top" alt="..." />
               <div class="card-body">
-                <p class="card-text">29 Different Types of Science books...</p>
+               
               </div>
-              <div class="btn btn-outline-secondary">View More</div>
+             
             </div>
           </div>
           <div
-            class="col-md-4 d-flex justify-content-center align-items-center"
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img/NTSdzBIDpp.jpg" class="card-img-top" alt="..." />
+              <img src="https://m.media-amazon.com/images/I/71uTbAOpdZL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
-                <p class="card-text">20 Different Types of fiction books...</p>
+                
               </div>
-              <div class="btn btn-outline-secondary">View More</div>
+              
             </div>
           </div>
         </div>
         <div class="row mt-5">
           <div
-            class="col-md-4 d-flex justify-content-center align-items-center"
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img/adventure.jpg" class="card-img-top" alt="..." />
+              <img src="https://images-na.ssl-images-amazon.com/images/I/71DRSBvfEmL._AC_UL600_SR600,600_.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
-                <p class="card-text">
-                  19 Different Types of adventure books...
-                </p>
+               
               </div>
-              <div class="btn btn-outline-secondary">View More</div>
+              
             </div>
           </div>
           <div
-            class="col-md-4 d-flex justify-content-center align-items-center"
+            class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img/bio.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO_HM6I7H6AmtIsCLcnY8vlALkhQGJFyueXdPsz74Hhg&s" class="card-img-top" alt="..." />
               <div class="card-body">
-                <p class="card-text">
-                  10 Different Types of biography books...
-                </p>
+                
               </div>
-              <div class="btn btn-outline-secondary">View More</div>
+              
             </div>
           </div>
           <div
-            class="col-md-4 d-flex justify-content-center align-items-center"
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img/commic.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHKIsnmxVvnTem4hC65OJ73M43nc1ebYQahn1Lvy9OZX-o5eNA3SPVNhdbitsXvADQg8c&usqp=CAU" class="card-img-top" alt="..." />
               <div class="card-body">
-                <p class="card-text">25 Different Types of commic books...</p>
+              
               </div>
-              <div class="btn btn-outline-secondary">View More</div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFoiKB2w34oypEfAe_1l1vJGJ2VHchrmvXJLQLOMz-gOQhhTQEF7c9LA8461geoWcWETQ&usqp=CAU" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://m.media-amazon.com/images/I/41FW38zKFNL.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://m.media-amazon.com/images/I/61YewZXWLYL._AC_UF350,350_QL50_.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://m.media-amazon.com/images/I/71QwPUc2HtL._AC_UF1000,1000_QL80_.jpg" />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR83LfwwN4oVvC0IjZGAAUdgZvPWS1CazfL-rgnpVz6Tg&s" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JwWNCUoIXmaa6EN6pEI1tEQRNIROqGwbAbC2-sMAUA&s" class="card-img-top" alt="..." />
+              <div class="card-body">
+              
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://cdn.thestorygraph.com/vdl3kdyw3g6lcshg5o8sr7s24i43" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+              
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1KJhnVtY5tM1SdvmWpjq07ElVUti9xHSJzzjuGpOMUKbClyw55HB-XZRfKLMy0RZD3NE&usqp=CAU" class="card-img-top" alt="..." />
+              <div class="card-body">
+               
+              </div>
+             
+            </div>
+          </div>
+          <div
+            class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
+          >
+            <div class="card p-2" style="width: 18rem">
+              <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1407757142i/22892415.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                
+              </div>
+              
             </div>
           </div>
         </div>
       </div>
+      
     </section>
-<!-- PROGRAMMING BOOK SECTION END--> 
+<!--FECTIONBOOKS END -->
+
   
-<!-- THIS IS FOOTER SECTION -->
+<!-- THIS IS FOOTER SECTION START -->
  <?php 
         require_once("footer.php");
     ?>
-<!-- THIS IS FOOTER SECTION -->
+<!-- THIS IS FOOTER SECTION END -->
  
