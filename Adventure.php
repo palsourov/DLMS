2d59f0e56f3f89aec9c9487dee7bb0ccdfbf10a9
palsourov <palsourov365@gmail.com>
2024-05-07 12:44:48 +0600

    Add(update code Adventure.php)

--- Adventure.php
+++ Adventure.php
@@ -1,10 +1,10 @@
 <!-- THIS IS HEADER SECTION -->
-<?php 
+   <?php 
         require_once("header.php");
     ?>
 <!-- THIS IS HEADER SECTION END-->
 
-<!-- CalculusBooks -->
+<!-- ADVENTUREBooks -->
   <section class="book_section">
       <h1 style="text-align: center; margin-top: 70px;"> Adventure Books</h1>
       <p style="text-align:center">Location: SeL:5 ,row:4</p>
@@ -26,7 +26,7 @@
             class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/cal.jpg" class="card-img-top" alt="..." />
+              <img src="https://images-cdn.reedsy.com/discovery/image/512/image/medium_9718449ab965b52c8c40ce526f7b2c86b6d25b22.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
                
               </div>
@@ -37,7 +37,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c3.jpg" class="card-img-top" alt="..." />
+              <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348243806i/310146._UX160_.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
                 
               </div>
@@ -50,7 +50,7 @@
             class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c4.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_WUZHToPpuH2nTi-6bmH1wqydB5aiP4f_s67I-bf3fpmZp5cnCNuk51Sx99sUReIICI&usqp=CAU" class="card-img-top" alt="..." />
               <div class="card-body">
                
               </div>
@@ -61,7 +61,7 @@
             class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c5.jpg" class="card-img-top" alt="..." />
+              <img src="https://m.media-amazon.com/images/I/51C7-UZfMQS.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
                 
               </div>
@@ -72,7 +72,7 @@
             class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c6.jpg" class="card-img-top" alt="..." />
+              <img src="https://m.media-amazon.com/images/I/71vvc0J-6VL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
               
               </div>
@@ -85,7 +85,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c7.jpg" class="card-img-top" alt="..." />
+              <img src="https://images-na.ssl-images-amazon.com/images/I/717iJ5lDZQL._AC_UL600_SR600,600_.jpg" class="card-img-top" alt="..." />
               <div class="card-body">
                
               </div>
@@ -96,7 +96,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c8.jpg" class="card-img-top" alt="..." />
+              <img src="https://cdn2.momjunction.com/wp-content/uploads/2015/12/10-Best-Adventure-Books-For-Teens-To-Read-1.jpg.webp" class="card-img-top" alt="..." />
               <div class="card-body">
                
               </div>
@@ -107,7 +107,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c9.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWkAIAn_o1EcNzhLqzlwZQaNK1F-cEOm8vYYkcar6E0qcrwViKZB7jKGrbbMPZUFP7Cp0&usqp=CAU" class="card-img-top" alt="..." />
               <div class="card-body">
                 
               </div>
@@ -120,7 +120,7 @@
             class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c4.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO7TuaamtL2l4tm-uAc1pzwGQQp60k7Gmz0V0Jsf0t-A&s" />
               <div class="card-body">
                
               </div>
@@ -131,7 +131,7 @@
             class="  col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c5.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNFC7qNmMAuxSWC_C-wkfjEPFigztw3XQ5f1BDESN3lA&s" class="card-img-top" alt="..." />
               <div class="card-body">
                 
               </div>
@@ -142,7 +142,7 @@
             class=" col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c6.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMDh3ySel8thnMNP1BYC9Qyn0a1BgyYpNzwOmpN-NGuw&s" class="card-img-top" alt="..." />
               <div class="card-body">
               
               </div>
@@ -155,7 +155,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c7.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_WUZHToPpuH2nTi-6bmH1wqydB5aiP4f_s67I-bf3fpmZp5cnCNuk51Sx99sUReIICI&usqp=CAU" class="card-img-top" alt="..." />
               <div class="card-body">
                
               </div>
@@ -166,7 +166,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c8.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtW0sAL8i-ceGS2cHoEwmrAbZu1rGN27U2MLubJQMYj9Qs-B5ljTzNTtasZn8gkAyjFQI&usqp=CAU" class="card-img-top" alt="..." />
               <div class="card-body">
                
               </div>
@@ -177,7 +177,7 @@
             class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
           >
             <div class="card p-2" style="width: 18rem">
-              <img src="./img./mathBookImg/c9.jpg" class="card-img-top" alt="..." />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAT4bjXRWsI3RjpU8EB2vklq30-G8FJoSZNGHR7TcYAw&s" class="card-img-top" alt="..." />
               <div class="card-body">
                 
               </div>
@@ -188,7 +188,7 @@
       </div>
       
     </section>
-<!-- CALCULUS BOOKS END -->
+<!--ADVENTUREBOOKS END -->
 
   
 <!-- THIS IS FOOTER SECTION -->
