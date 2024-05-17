9b20698edf1efd3e7a0b9cb159e51d57aa47b397
pdp43 <dasproyash10@gmail.com>
2024-05-17 16:18:08 +0600

    contact

---
+++ Contac.php
@@ -0,0 +1,21 @@
+<?php 
+        require_once("header.php");
+    ?>
+<div class="contact">
+    <div class="container ">
+        <h2>Contact</h2>
+        <div class="row ">
+        <div class="col-md-6 mt-3">
+            <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.0565631142904!2d91.97044227358475!3d24.930142942469097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3750552bc71c899d%3A0x804e438bcc32b390!2sMetropolitan%20University%20Sylhet!5e0!3m2!1sen!2sbd!4v1715938530518!5m2!1sen!2sbd" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        <div class="col-md-6 mt-3">
+            <div class="contactPart" >
+                <h3>Metropolitan University <h3>
+       
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
